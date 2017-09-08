43c19e3abbf0a52544298d19528c7ba62606fd45
Elena Tolpygo Cranley <elena.cranley@ucsf.edu>
2017-09-08 15:57:03 -0700

    import forgotten reverse into snippets/views

--- snippets/views.py
+++ snippets/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics, permissions, renderers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
 from snippets.permissions import IsOwnerOrReadOnly
 
 
